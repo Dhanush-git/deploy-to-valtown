a7b36697ad5b71393bebf8c66d807f01f5ae7c32
Dhanush Suvarna <suvarnadhanush2000@gmail.com>
2025-04-12 00:38:24 +0530

    Update project configuration: added .env to .gitignore, modified build script in package.json, and implemented main functionality in src/main.ts to read configuration from valtown.json.

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,21 @@
 import ValTown from "@valtown/sdk";
-import * as core from "@actions/core";
+import * as fs from "fs";
+import * as path from "path";
 
 const valTown = new ValTown({
-  bearerToken: core.getInput("VAL_TOWN_API_KEY"),
+  bearerToken: process.env.VAL_TOWN_API_KEY,
 });
 
-async function main() {
+try {
+  const configPath = path.join(process.env.GITHUB_WORKSPACE, "valtown.json");
+  const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
+  console.log(config);
+} catch (error) {
+  console.error(error);
+  process.exit(1);
+}
+
+export async function main() {
   const me = await valTown.me.profile.retrieve();
   console.log(me);
 }
-
-main();
