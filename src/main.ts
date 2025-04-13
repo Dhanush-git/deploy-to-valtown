0c9cf9b1c799c8f8ea5d0ffd6b866451983fae00
Dhanush Suvarna <suvarnadhanush2000@gmail.com>
2025-04-13 11:46:34 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 // import ValTown from "@valtown/sdk";
 import * as fs from "fs";
 import * as path from "path";
+import * as core from "@actions/core";
 
-console.log(__dirname);
+const valTownToken = core.getInput("VAL_TOWN_TOKEN");
+
+if (!valTownToken || valTownToken === "") {
+  core.setFailed("VAL_TOWN_TOKEN is not set or is empty");
+  process.exit(1);
+}
+
+const vs = fs.readFileSync(path.join(__dirname, "../valtown.json"), "utf8");
+// if file does not exist, throw error
+if (!fs.existsSync(path.join(__dirname, "../valtown.json"))) {
+  core.setFailed("valtown.json does not exist");
+  process.exit(1);
+}
+
