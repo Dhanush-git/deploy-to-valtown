b0157a267faa4af4cd8a57815c0757f651370861
Dhanush Suvarna <suvarnadhanush2000@gmail.com>
2025-04-13 11:10:06 +0530

    update

--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,11 @@
 import ValTown from "@valtown/sdk";
-import * as fs from "fs";
-import * as path from "path";
+import * as core from "@actions/core"
 
-if (!process.env.VAL_TOWN_API_KEY) {
-  console.error("VAL_TOWN_API_KEY is not set");
+
+const valTownToken = core.getInput("valtown-token");
+
+// check if valTownToken is set
+if (!valTownToken) {
+  core.setFailed("valtown-token is not set");
   process.exit(1);
-}
-
-const valTown = new ValTown({
-  bearerToken: process.env.VAL_TOWN_API_KEY,
-});
-
-try {
-  const configPath = path.join("./", "valtown.json");
-  const config = JSON.parse(fs.readFileSync(configPath, "utf8"));
-  console.log(config);
-} catch (error) {
-  console.error(error);
-  process.exit(1);
-}
-
-export async function main() {
-  const me = await valTown.me.profile.retrieve();
-  console.log(me);
 }
